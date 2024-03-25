de6389f7cd572b34cadb294cf8737b189b14c3ce
Dustin <gjf66924011@gmail.com>
2024-03-25 18:05:30 +0800

    完成瓦片地图相关

--- Ruby/Assets/Scripts/RubyController.cs
+++ Ruby/Assets/Scripts/RubyController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+
+    public float speed = 5F;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,12 @@ public class RubyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector2 cur = transform.position;
+        cur.x += speed * horizontal * Time.deltaTime;
+        cur.y += speed * vertical * Time.deltaTime;
+        transform.position = cur;
     }
 }
